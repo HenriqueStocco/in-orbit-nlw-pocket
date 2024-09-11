b5ed8d7939c8e36cec091dd156bffb19e843b2fa
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-09-11 13:23:32 -0300

    fix: remove other envs

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,6 @@
-import z from 'zod'
+import z from "zod"
 
 const envSchema = z.object({
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_DB: z.string(),
   DATABASE_URL: z.string().url(),
 })
 
