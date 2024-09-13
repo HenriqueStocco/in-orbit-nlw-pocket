c2a8612d05e99c11746cad245269743c7cd76fbc
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-09-13 13:03:38 -0300

    feat: add completions route

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,6 +8,7 @@ import z from "zod"
 
 import { craeteGoal } from "../functions/create-goal"
 import { getWeekPendingGoals } from "../functions/get-week-pending-goals"
+import { createGoalCompletion } from "../functions/create-goal-completion"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -30,6 +31,24 @@ app.post(
     const createGoalSchema = await craeteGoal({
       title,
       desiredWeeklyFrequency,
+    })
+  }
+)
+
+app.post(
+  "/completions",
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+
+    await createGoalCompletion({
+      goalId,
     })
   }
 )
